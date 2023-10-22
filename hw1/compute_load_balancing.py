985da89670d7a23b8b69908b49c10a7138d9a8fc
朱季葳 <wewe5215@zhujiweideMacBook-Pro.local>
2023-10-23 01:42:51 +0800

    feat : re-allocate all the experiment result and add Report generated by Nsight system

---
+++ hw1/compute_load_balancing.py
@@ -0,0 +1,34 @@
+import re
+with open('mpip_37_100000000.txt', 'r') as file:
+    lines = file.readlines()
+tasks = []
+apptimes = []
+mpitimes = []
+precent = []
+for line in lines:
+    line = line.strip().split('\n')[0]
+    values = re.split(r'\s+', line.strip())
+    task = values[0]
+    tasks.append(task)
+    # print('task = ', task)
+    AppTime = values[1]
+    apptimes.append(AppTime)
+    # print('AppTime = ', AppTime)
+    MPITime = values[2]
+    mpitimes.append(MPITime)
+    # print('MPITime = ', MPITime)
+    MPITime_pc = values[3]
+    precent.append(MPITime_pc)
+    # print('MPITime_pc = ', MPITime_pc)
+print('task:')
+for t in tasks:
+    print(t)
+print('apptime:')
+for t in apptimes:
+    print(t)
+print('mpitime:')
+for t in mpitimes:
+    print(t)
+print('percent:')
+for t in precent:
+    print(t)
---
+++ hw1/download_data.sh
@@ -0,0 +1,3 @@
+FILENAME="/home/pp23/pp23s25/HW1/nsight"
+scp -r pp23s25@apollo.cs.nthu.edu.tw:${FILENAME} .
+# scp pp23s25@apollo.cs.nthu.edu.tw:${FILENAME} .
